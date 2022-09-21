7805e5d0b553f3dbca005ac5b2471c60a0dd629f
asif-sidd9768 <siddiqueasif421@gmail.com>
2022-09-21 17:21:59 +0530

    Negative and formula correct

--- app.js
+++ app.js
@@ -4,19 +4,41 @@ const noOfStocks = document.querySelector("#noOfStocks");
 const currentPrice = document.querySelector("#currentPrice");
 const answer = document.querySelector("#answer");
 
+const valueReset = () => {
+  initialPrice.value = ""
+  noOfStocks.value = ""
+  currentPrice.value = ""
+}
+
 const checkButtonHandler = () => {
-  const profit =
-    (currentPrice.valueAsNumber - initialPrice.valueAsNumber) *
-    noOfStocks.valueAsNumber;
-  const profitPercent = (profit / initialPrice.valueAsNumber) * 100;
+  const initial = initialPrice.valueAsNumber
+  const current = currentPrice.valueAsNumber
+  const stocks = noOfStocks.valueAsNumber
+  console.log(`initial = ${initial} ,,,, current ${current}`)
 
-  if (profit >= 0) {
-    answer.innerHTML = `Your Profit is ${profit} and percentage is ${profitPercent}%`;
-  } else {
-    answer.innerHTML = `Your loss is ${profit * -1} and percentage is ${
-      profitPercent * -1
-    }%`;
+  if(initial < 0 || current < 0 || stocks < 0){
+    answer.innerHTML = `Values can't be negative!`
+    valueReset()
+    return
   }
-};
 
+  if(initial > current) {
+    const loss = (initial - current) * stocks
+    const lossPercentage = (loss/initial) * 100
+    answer.innerHTML = `Your Loss is ${loss} and percentage is ${lossPercentage}%`
+    valueReset()
+    return 
+  }else if (initial < current){
+    const profit = (current - initial) * stocks;
+    const profitPercentage = (profit / initial) * 100;
+    answer.innerHTML = `Your Profit is ${profit} and percentage is ${profitPercentage}%`
+    valueReset()
+    return
+  }
+  else{
+    answer.innerHTML = `Do something in life!`
+    return
+  }
+
+};
 checkButton.addEventListener("click", checkButtonHandler);
